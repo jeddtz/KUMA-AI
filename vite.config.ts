3a27a337e005ccba7f423c32641fdc7f73147609
jeddtz <hafidzmuhammad536@gmail.com>
2026-07-23 18:48:12 +0700

    fix: use vite-plugin-node-polyfills to support backend modules in frontend

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
+import {nodePolyfills} from 'vite-plugin-node-polyfills';
 
 export default defineConfig(() => {
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss(), nodePolyfills()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
